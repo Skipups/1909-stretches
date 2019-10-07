25cc18d40e7f954e4084a63d2227347d8f705d60
Skipups <svetlana.rovinsky@gmail.com>
2019-10-07 18:54:05 -0400

    not complete

--- questions/05_Query/query.js
+++ questions/05_Query/query.js
@@ -1,19 +1,19 @@
 const items = [
   {
     id: 1,
-    name: 'foo',
-    price: 2,
+    name: "foo",
+    price: 2
   },
   {
     id: 3,
-    name: 'bar',
-    price: 4,
+    name: "bar",
+    price: 4
   },
   {
     id: 66,
-    name: 'bazz',
-    price: 4,
-  },
+    name: "bazz",
+    price: 4
+  }
 ];
 
 // query(items, { name: 'foo' })
@@ -44,6 +44,21 @@ const items = [
 //       price: 4,
 //   }]
 
-function query(arr, obj) {}
+function query(arr, obj) {
+  console.log("arr", arr);
+  console.log("obj", obj);
+  return arr.reduce((accum, curr) => {
+    console.log("curr", curr);
+    console.log("accum", accum);
+    for (let key in obj) {
+      if (obj[key] === curr[key]) {
+        accum += curr;
+        console.log("curr", curr);
+        console.log("accum", accum);
+      }
+    }
+    return accum;
+  }, {});
+}
 
 module.exports = { query, items };
