f719edb95ea8ae5710473344ae4ab585edf71d5d
Skipups <svetlana.rovinsky@gmail.com>
2019-10-16 18:56:44 -0400

    couldn't solve

--- questions/08_groupBy/groupBy.js
+++ questions/08_groupBy/groupBy.js
@@ -1,3 +1,23 @@
-function groupBy(arr, attr) {}
+function groupBy(arr, attr) {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    let elm = arr[i];
+    //console.log(attr(elm));
+    obj[elm] += elm.attr;
+  }
+  console.log(obj);
+  return obj;
+}
 
 module.exports = { groupBy };
+
+// function groupBy(arr, attr) {
+//     let obj = {};
+//     for (let i = 0; i < arr.length; i++) {
+//       let elm = arr[i];
+//       console.log(elm, attr);
+//       obj[elm] += elm.attr;
+//     }
+//     console.log(obj);
+//     return obj;
+//   }
