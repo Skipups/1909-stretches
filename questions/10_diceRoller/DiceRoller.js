64fcd3acc494e0e49bb3813df7c2766177a562f4
Skipups <svetlana.rovinsky@gmail.com>
2019-10-23 18:57:39 -0400

    not done

--- questions/10_diceRoller/DiceRoller.js
+++ questions/10_diceRoller/DiceRoller.js
@@ -3,7 +3,19 @@
 // history property which outputs all of the previous rolls
 
 class DiceRoller {
-  //YOUR CODE
+  constructor(sides, numDice) {
+    if (sides === 0 || side < 1 || numDice < 1) throw console.error("error");
+    else this.sides = sides;
+    this.numDice = numDice;
+    this.rollHistory = [];
+  }
+  roll() {
+    return this.rollHistory.push(this.sides * this.numDice);
+  }
+  history() {
+    console.log(this.rollHistory.join(","));
+    return this.rollHistory.join(",");
+  }
 }
 
 module.exports = { DiceRoller };
