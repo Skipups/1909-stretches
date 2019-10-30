84198d95d1b4eb6b8ad0b5aedc8c60ac51a7f640
Skipups <svetlana.rovinsky@gmail.com>
2019-10-30 18:51:06 -0400

    done

--- questions/13_multiply/multiply.js
+++ questions/13_multiply/multiply.js
@@ -1,3 +1,9 @@
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+  if (typeof num1 !== "number" && typeof num2 !== "number") {
+    throw new Error("must be a number");
+  } else {
+    return num1 * num2;
+  }
+};
 
 module.exports = { multiply };
