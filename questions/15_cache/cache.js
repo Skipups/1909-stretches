90e76678a3dc0f3e76a4a65166f3ecf1ab8886aa
Skipups <svetlana.rovinsky@gmail.com>
2019-11-13 18:56:12 -0500

    not done

--- questions/15_cache/cache.js
+++ questions/15_cache/cache.js
@@ -1,3 +1,18 @@
-function cache() {}
+function cache(func) {
+  let numOptions = [];
+  let result;
+  if (typeof func !== "function") throw Error("Input must be a function.");
+  else {
+    return num => {
+      if (!numOptions.includes(num)) {
+        numOptions.push(num);
+        let result = func(num);
+        return result;
+      } else {
+        return result;
+      }
+    };
+  }
+}
 
 module.exports = { cache };
