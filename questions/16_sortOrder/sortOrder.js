8b7eb7db980765ac6ac149786eeefec805bf9bd3
Skipups <svetlana.rovinsky@gmail.com>
2019-11-18 19:01:25 -0500

    nope

--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -3,6 +3,19 @@ SortOrder takes an array of objects, a key to be sorted on
 and a key for the return value. 
 */
 
-function sortOrder() {}
+function sortOrder(hsort, data){
+    if (typeOf hsort === "number"){
+        data.reduce( (prev, curr) => {
+            console.log(curr)
+            if(prev[sorts] >curr[sorts] ){
+                retu
+            }
+            return prev
+        },[])
+        data.forEach( obj => {
+            Object.keys(obj)
+        })
+    }
+}
 
 module.exports = { sortOrder };
